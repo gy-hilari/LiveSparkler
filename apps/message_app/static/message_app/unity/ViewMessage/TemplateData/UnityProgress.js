9cbde22ef01412ee2df2be7714209187ba013d03
Gabe H <gy.hilari@gmail.com>
2019-06-28 00:56:08 -0700

    Fixed stack overflow (last array element) error!

--- apps/message_app/static/message_app/unity/ViewMessage/TemplateData/UnityProgress.js
+++ apps/message_app/static/message_app/unity/ViewMessage/TemplateData/UnityProgress.js
@@ -26,14 +26,18 @@ function UnityProgress(gameInstance, progress) {
     initUnity();
   }, 2500);
 
+
+ 
+
+
   function initUnity() {
     countMax = document.getElementById("fwTotal").getAttribute("value");
     console.log(countMax);
 
     var i = 0;
-    while (i < fwCategories.length - 1) {
+    while (i <= fwCategories.length - 1) {
       if (document.getElementById("fwType_" + fwCategories[i])) {
-        for (v = 0; v <= (document.getElementById("fwCount_" + fwCategories[i]).getAttribute("value")) - 1; v++) {
+        for (v = 0; v <= (document.getElementById("fwCount_" + fwCategories[i]).getAttribute("value")); v++) {
           console.log("%*%*%%*%*%*%**%*%*%*%*%%*")
           console.log("fwType_" + fwCategories[i])
           gameInstance.SendMessage("FireworkSpawner", "loadSequence", ("fwType_" + fwCategories[i]));
